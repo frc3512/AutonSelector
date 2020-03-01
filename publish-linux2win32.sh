936110954bae9d58577778af2b4649af82641c37
Tyler Veness <calcmogul@gmail.com>
2020-03-01 13:32:37 -0800

    Add missing dll to publish

--- publish-linux2win32.sh
+++ publish-linux2win32.sh
@@ -16,7 +16,7 @@ rm -rf $PROJECT-$DATE $PROJECT-$DATE.zip
 mkdir -p $PROJECT-$DATE/platforms
 
 # Copy files into folder
-cp build/$PROJECT.exe /usr/$PREFIX/bin/{Qt5Core,Qt5Gui,Qt5PrintSupport,Qt5Network,Qt5Widgets,libbz2-1,libfreetype-6,libgcc_s_seh-1,libglib-2.0-0,libgraphite2,libharfbuzz-0,libiconv-2,libintl-8,libjpeg-8,libpcre-1,libpcre2-16-0,libpng16-16,libstdc++-6,libwinpthread-1,zlib1}.dll IPSettings.txt $PROJECT-$DATE
+cp build/$PROJECT.exe /usr/$PREFIX/bin/{Qt5Core,Qt5Gui,Qt5PrintSupport,Qt5Network,Qt5Widgets,libbz2-1,libfreetype-6,libgcc_s_seh-1,libglib-2.0-0,libgraphite2,libharfbuzz-0,libiconv-2,libintl-8,libjpeg-8,libpcre-1,libpcre2-16-0,libpng16-16,libssp-0,libstdc++-6,libwinpthread-1,zlib1}.dll IPSettings.txt $PROJECT-$DATE
 cp /usr/$PREFIX/lib/qt/plugins/platforms/qwindows.dll $PROJECT-$DATE/platforms
 $PREFIX-strip $PROJECT-$DATE/$PROJECT.exe
 
--- publish-msys2win32.sh
+++ publish-msys2win32.sh
@@ -15,7 +15,7 @@ rm -rf $PROJECT-$DATE $PROJECT-$DATE.zip
 mkdir -p $PROJECT-$DATE/platforms
 
 # Copy files into folder
-cp build/$PROJECT.exe /mingw64/bin/{Qt5Core,Qt5Gui,Qt5PrintSupport,Qt5Network,Qt5Widgets,libbz2-1,libfreetype-6,libgcc_s_seh-1,libglib-2.0-0,libgraphite2,libharfbuzz-0,libiconv-2,libintl-8,libjpeg-8,libpcre-1,libpcre2-16-0,libpng16-16,libstdc++-6,libwinpthread-1,zlib1}.dll IPSettings.txt $PROJECT-$DATE
+cp build/$PROJECT.exe /mingw64/bin/{Qt5Core,Qt5Gui,Qt5PrintSupport,Qt5Network,Qt5Widgets,libbz2-1,libfreetype-6,libgcc_s_seh-1,libglib-2.0-0,libgraphite2,libharfbuzz-0,libiconv-2,libintl-8,libjpeg-8,libpcre-1,libpcre2-16-0,libpng16-16,libssp-0,libstdc++-6,libwinpthread-1,zlib1}.dll IPSettings.txt $PROJECT-$DATE
 cp /mingw64/share/qt5/plugins/platforms/qwindows.dll $PROJECT-$DATE/platforms
 strip $PROJECT-$DATE/$PROJECT.exe
 
